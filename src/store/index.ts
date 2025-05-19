370955dea43ab378726aaed8b19609632a909c35
Itu Bhawal <bhawalitu23@gmail.com>
2025-05-20 00:24:13 +0530

    redux dev tools extension setup

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,12 @@
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import { quizReducer } from "./reducers/quizReducer";
 import logger from "./middleware/logger";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(quizReducer, applyMiddleware(logger));
+const store = createStore(
+  quizReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
 export type RootState = ReturnType<typeof store.getState>;
 
